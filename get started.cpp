89ccf056fb519caf5a0747de9b779b6989aa44fe
TerraBladeSky <sky.ryder.bro.100@gmail.com>
2025-09-10 14:37:48 +0800

    comments added

--- get started.cpp
+++ get started.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello World!";
-    return 0;
-}
+#include <iostream>                 //header file library
+using namespace std;                //means we can use names for objects and variables from the standard library
+                                    //blacnk line for better readability
+int main() {                        //this is a function
+    cout << "Hello World!";         //object used together with the insertion operator to output/print text
+    return 0;                       //ends the main function
+}                                   //dont forget to add closing curly brackt to actually end the function
