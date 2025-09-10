2c2a4778a4be662a033aaac3f17568d2ef989d72
TerraBladeSky <sky.ryder.bro.100@gmail.com>
2025-09-10 15:09:49 +0800

    comments section complete

---
+++ C++ Multi-line Comments.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*Multi-line comments start with the slash then the asterisk, 
+    and ends with the asterisk then the slash.
+    Any text between the astart and end slashes will be ignored by the compiler.*/
+    
+    /*the code below will print the words Hello World!
+    to the screen,*/
+    cout << "Hello World!";
+    return 0;
+}
