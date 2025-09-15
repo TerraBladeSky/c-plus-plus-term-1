86f7fdc783a2ca8f93fb10c6996d5df6cd4c3e83
TerraBladeSky <sky.ryder.bro.100@gmail.com>
2025-09-15 15:31:48 +0800

    swtich section done

---
+++ C++ Switch.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    //switch
+    //switch statements
+    switch(expression) {
+        case x:
+            // code block
+            break;
+        case y:
+            // code block
+            break;
+        default:
+            // code block
+    }
+
+    cout << endl;
+
+    /*This is how it works:
+    The switch expression is evaluated once
+    The value of the expression is compared 
+    with the values of each case
+    If there is a match, the associated block 
+    of code is executed
+    The break and default keywords are optional, 
+    and will be described later*/
+
+    //eg
+    int day = 4;
+    switch (day) {
+        case 1:
+            cout << "Monday";
+            break;
+        case 2:
+            cout << "Tuesday";
+            break;
+        case 3:
+            cout << "Wednesday";
+            break;
+        case 4:
+            cout << "Thursday";
+            break;
+        case 5:
+            cout << "Friday";
+            break;
+        case 6:
+            cout << "Saturday";
+            break;
+        case 7:
+            cout << "Sunday";
+            break;
+    }
+
+    // outputs "Thursday" (day 4)
+
+    cout << endl;
+
+    //end
+
+    cout << endl;
+
+    return 0;
+
+}
