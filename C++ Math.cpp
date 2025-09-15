3c2300970f1ccc1aac2149db16232d673a907bc4
TerraBladeSky <sky.ryder.bro.100@gmail.com>
2025-09-15 13:27:56 +0800

    math section done

---
+++ C++ Math.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    //math
+    //max and min
+    cout << max(5,10);
+
+    cout << min(5,10);
+
+    cout << endl;
+
+    /*math librery:
+    // Include the cmath library
+    #include <cmath>
+
+    cout << sqrt(64);
+    cout << round(2.6);
+    cout << log(2);*/
+    //end
+
+    cout << endl;
+
+    return 0;
+
+}
